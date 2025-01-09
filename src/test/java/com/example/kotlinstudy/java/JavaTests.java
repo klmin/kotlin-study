81b42867dd2724773370b55985c12be7eeda3cee
klmin <haemmas6394@gmail.com>
2025-01-09 17:58:15 +0900

    Add test cases

--- src/test/java/com/example/kotlinstudy/java/JavaTests.java
+++ src/test/java/com/example/kotlinstudy/java/JavaTests.java
@@ -4,9 +4,12 @@ import com.example.kotlinstudy.java.entity.Member;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 @Slf4j
@@ -18,39 +21,95 @@ public class JavaTests {
     @Test
     void 자바_객체_테스트() throws JsonProcessingException {
 
-        var idChangeEntity = Member.builder()
-                                   .id(200L)
-                                   .name("testName")
-                                   .age(30)
-                                   .build();
-        log.info("idChangeEntity : {}", idChangeEntity);
+        Long id = 200L;
+        String name = "testName";
+        Integer age = 30;
 
+        var idChangeEntity = Member.builder()
+                                   .id(id)
+                                   .name(name)
+                                   .age(age)
+                                   .build();
+
+        Assertions.assertEquals(id, idChangeEntity.getId());
+        Assertions.assertEquals(name, idChangeEntity.getName());
+        Assertions.assertEquals(age, idChangeEntity.getAge());
+        Assertions.assertNull(idChangeEntity.getDescription());
 
         String writeAsString = objectMapper.writeValueAsString(idChangeEntity);
-        log.info("writeAsString : {}", writeAsString);
+        Assertions.assertNotNull(writeAsString);
 
-        var readValue = objectMapper.readValue(writeAsString, Member.class);
-        log.info("readValue : {}", readValue);
+        var readValueMember = objectMapper.readValue(writeAsString, Member.class);
 
-        var convertValue = objectMapper.convertValue(idChangeEntity, Member.class);
-        log.info("convertValue : {}", convertValue);
+        assertThat(idChangeEntity).usingRecursiveComparison().isEqualTo(readValueMember);
+
+        var convertValueMember = objectMapper.convertValue(idChangeEntity, Member.class);
+        assertThat(readValueMember).usingRecursiveComparison().isEqualTo(convertValueMember);
+
+        id = 1L;
+        name = "testName2";
+        age = 20;
 
         var idNameChangeEntity = new Member(1L, "testName2", 20, null);
-        log.info("idNameChangeEntity : {}", idNameChangeEntity);
+        Assertions.assertEquals(id, idNameChangeEntity.getId());
+        Assertions.assertEquals(name, idNameChangeEntity.getName());
+        Assertions.assertEquals(age, idNameChangeEntity.getAge());
+        Assertions.assertNull(idNameChangeEntity.getDescription());
+
+        id = 2L;
+        name = "testName3";
+        String description = "test_description";
 
         var copyAndNameChangeEntity = idNameChangeEntity.toBuilder()
-                                                        .id(2L)
-                                                        .name("testName3")
-                                                        .description("test_description")
+                                                        .id(id)
+                                                        .name(name)
+                                                        .description(description)
                                                         .build();
-        log.info("copyAndNameChangeEntity : {}", copyAndNameChangeEntity);
+
+        Assertions.assertEquals(id, copyAndNameChangeEntity.getId());
+        Assertions.assertEquals(name, copyAndNameChangeEntity.getName());
+        Assertions.assertEquals(age, copyAndNameChangeEntity.getAge());
+        Assertions.assertEquals(description, copyAndNameChangeEntity.getDescription());
+
+        id = 3L;
 
         var copyAndIdChangeEntity = copyAndNameChangeEntity.toBuilder().id(3L).build();
-        log.info("copyAndIdChangeEntity : {}", copyAndIdChangeEntity);
+        Assertions.assertEquals(id, copyAndIdChangeEntity.getId());
+        Assertions.assertEquals(copyAndNameChangeEntity.getName(), copyAndIdChangeEntity.getName());
+        Assertions.assertEquals(copyAndNameChangeEntity.getAge(), copyAndIdChangeEntity.getAge());
+        Assertions.assertEquals(copyAndNameChangeEntity.getDescription(), copyAndIdChangeEntity.getDescription());
 
+        age = 10;
 
         var copyAndAgeChangeEntity = copyAndIdChangeEntity.toBuilder().age(10).build();
-        log.info("copyAndAgeChangeEntity : {}", copyAndAgeChangeEntity);
+        Assertions.assertEquals(copyAndIdChangeEntity.getId(), copyAndAgeChangeEntity.getId());
+        Assertions.assertEquals(copyAndIdChangeEntity.getName(), copyAndAgeChangeEntity.getName());
+        Assertions.assertEquals(age, copyAndAgeChangeEntity.getAge());
+        Assertions.assertEquals(copyAndIdChangeEntity.getDescription(), copyAndAgeChangeEntity.getDescription());
+
 
     }
+
+    @Test
+    void 널러블_타입_테스트(){
+
+        String name = null; // ✅ Null을 허용
+
+        // ✅ 컴파일 성공
+        // ❌ 런타임에서 실행시 NPE 발생
+        Assertions.assertThrows(NullPointerException.class, () -> name.length());
+    }
+
+    @Test
+    void 안전_호출_연산자_테스트(){
+
+        String name = null; // ✅ Null을 허용
+
+        // ✅ 컴파일 성공
+        // ❌ 런타임에서 실행시 NPE 발생
+        Assertions.assertThrows(NullPointerException.class, () -> name.length());
+        Assertions.assertNull(name == null ? null : name.length());
+
+    }
+
 }
--- src/test/kotlin/com/example/kotlinstudy/kt/KotlinTests.kt
+++ src/test/kotlin/com/example/kotlinstudy/kt/KotlinTests.kt
@@ -4,6 +4,8 @@ package com.example.kotlinstudy.kt
 import com.example.kotlinstudy.kt.entity.Member
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import io.github.oshai.kotlinlogging.KotlinLogging
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
@@ -18,29 +20,100 @@ class KotlinTests {
     @DisplayName("코틀린 객체 테스트")
     fun 코틀린_객체_테스트() {
 
-        val idChangeEntity = Member(id=200L, name = "testName", age=30, description = null)
-        logger.info { "idChangeEntity : $idChangeEntity" }
+        var id : Long = 200L
+        var name : String = "testName"
+        var age : Int = 30
+        var description : String? = null
+
+        val idChangeEntity = Member(id=id, name = name, age=age, description = description)
+        Assertions.assertEquals(id, idChangeEntity.id)
+        Assertions.assertEquals(name, idChangeEntity.name)
+        Assertions.assertEquals(age, idChangeEntity.age)
+        Assertions.assertNull(idChangeEntity.description)
 
         val writeAsString = objectMapper.writeValueAsString(idChangeEntity);
-        logger.info { "writeAsString : $writeAsString" }
+        Assertions.assertNotNull(writeAsString)
 
-        val readValue = objectMapper.readValue(writeAsString, Member::class.java)
-        logger.info { "readValue : $readValue" }
+        val readValueMember = objectMapper.readValue(writeAsString, Member::class.java)
 
-        val convertValue = objectMapper.convertValue(idChangeEntity, Member::class.java)
-        logger.info { "convertValue : $convertValue" }
+        assertThat(idChangeEntity).usingRecursiveComparison().isEqualTo(readValueMember)
 
-        val idNameChangeEntity = Member(1, "testName2", 20, null)
-        logger.info { "idNameChangeEntity : $idNameChangeEntity" }
+        val convertValueMember = objectMapper.convertValue(idChangeEntity, Member::class.java)
 
-        val copyAndNameChangeEntity = idNameChangeEntity.copy(id=2, name="testName3", description = "test_description")
-        logger.info { "copyAndNameChangeEntity : $copyAndNameChangeEntity" }
+        assertThat(convertValueMember).usingRecursiveComparison().isEqualTo(readValueMember)
 
-        val copyAndIdChangeEntity = copyAndNameChangeEntity.copy(id=3L)
-        logger.info { "copyAndIdChangeEntity : $copyAndIdChangeEntity" }
+        id = 1
+        name = "testName2"
+        age = 20
+        description = null
 
-        val copyAndAgeChangeEntity = copyAndIdChangeEntity.copy(age=10)
-        logger.info { "copyAndAgeChangeEntity : $copyAndAgeChangeEntity" }
+        val idNameChangeEntity = Member(id, name, age, description)
+
+        Assertions.assertNotEquals(convertValueMember.id, idNameChangeEntity.id)
+        Assertions.assertNotEquals(convertValueMember.name, idNameChangeEntity.name)
+        Assertions.assertNotEquals(convertValueMember.age, idNameChangeEntity.age)
+
+        Assertions.assertEquals(id, idNameChangeEntity.id)
+        Assertions.assertEquals(name, idNameChangeEntity.name)
+        Assertions.assertEquals(age, idNameChangeEntity.age)
+        Assertions.assertEquals(convertValueMember.description, idNameChangeEntity.description)
+
+        id = 2
+        name = "testName3"
+        description = "test_description"
+
+        val copyAndNameChangeEntity = idNameChangeEntity.copy(id=id, name=name, description = description)
+        Assertions.assertNotEquals(idNameChangeEntity.id, copyAndNameChangeEntity.id)
+        Assertions.assertNotEquals(idNameChangeEntity.name, copyAndNameChangeEntity.name)
+        Assertions.assertNotEquals(idNameChangeEntity.description, copyAndNameChangeEntity.description)
+
+        Assertions.assertEquals(id, copyAndNameChangeEntity.id)
+        Assertions.assertEquals(name, copyAndNameChangeEntity.name)
+        Assertions.assertEquals(idNameChangeEntity.age, copyAndNameChangeEntity.age)
+
+        id = 3L
+        val copyAndIdChangeEntity = copyAndNameChangeEntity.copy(id=id)
+        Assertions.assertEquals(id, copyAndIdChangeEntity.id)
+        Assertions.assertNotEquals(copyAndNameChangeEntity.id, copyAndIdChangeEntity.id)
+        Assertions.assertEquals(copyAndNameChangeEntity.name, copyAndIdChangeEntity.name)
+        Assertions.assertEquals(copyAndNameChangeEntity.age, copyAndIdChangeEntity.age)
+        Assertions.assertEquals(copyAndNameChangeEntity.description, copyAndIdChangeEntity.description)
+
+        age = 10
+        val copyAndAgeChangeEntity = copyAndIdChangeEntity.copy(age=age)
+        Assertions.assertEquals(copyAndIdChangeEntity.id, copyAndAgeChangeEntity.id)
+        Assertions.assertEquals(copyAndIdChangeEntity.name, copyAndAgeChangeEntity.name)
+        Assertions.assertNotEquals(copyAndIdChangeEntity.age, copyAndAgeChangeEntity.age)
+        Assertions.assertEquals(age, copyAndAgeChangeEntity.age)
+        Assertions.assertEquals(copyAndIdChangeEntity.description, copyAndAgeChangeEntity.description)
+
+    }
+
+    @Test
+    fun 널러블_타입_테스트() {
+
+        val nullableName: String? = null // ✅ Null을 허용
+
+        // ❌ 컴파일 에러 발생 (개발시점에서 발견 가능)
+      //  var nonNullableName: String = null
+
+        var nonNullableString: String = "Hello"  // Null 허용 안됨
+        var nullableString: String? = "Hello"  // Null 허용 가능
+        nullableString = null  // ✅ 가능
+       // nonNullableString = null  // ❌ 오류 발생
+
+    }
+
+    @Test
+    fun 안전_호출_연산자_테스트(){
+
+        val name: String? = null
+        Assertions.assertNull(name?.length)
+
+        var str: String? = "Hello"
+
+        //println(str.length)   // ❌ 컴파일 오류 (Nullable 타입)
+        Assertions.assertEquals(5, str?.length)
 
     }
 
